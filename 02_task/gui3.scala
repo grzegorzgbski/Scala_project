f2a08815efc68f68c200ea9ad2f01571108d3395
grzegorzgbski <grzegorzgbski@gmail.com>
2017-06-04 14:11:23 +0200

    add

--- 02_task/gui3.scala
+++ 02_task/gui3.scala
@@ -2,10 +2,14 @@ import scala.swing._
 
 class UI extends MainFrame {
   title = "GUI Program #3"
-  contents = new BoxPanel(Orientation.Horizontal) {
+  contents = new BoxPanel(Orientation.Vertical) {
     contents += new Label("Look at me!")
+    contents += Swing.VStrut(10)
+    contents += Swing.Glue
     contents += Button("Press me, please") { println("Thank you") }
+    contents += Swing.VStrut(5)
     contents += Button("Close") { sys.exit(0) }
+    border = Swing.EmptyBorder(100, 100, 100, 100)
   }
 }
 
